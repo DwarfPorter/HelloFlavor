ff8fa50173594ece171274bf2520e1534a035243
DwarfPorter <mrvlmor@gmail.com>
2018-12-28 19:47:39 +0300

    Добавлен flavor с другим dimension view (demo, master)

---
+++ app/src/demo/java/ru/geekbrains/helloflavor/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.helloflavor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonOk = findViewById(R.id.buttonOk);
+        buttonOk.setEnabled(true);
+    }
+}
